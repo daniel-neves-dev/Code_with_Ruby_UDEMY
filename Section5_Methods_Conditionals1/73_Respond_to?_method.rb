e036adbd07834cc9dfd387aac5b743b339f706e0
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-06 18:25:53 -0300

    Respond_to

---
+++ Section5_Methods_Conditionals1/73_Respond_to?_method.rb
@@ -0,0 +1,9 @@
+p "string".respond_to?("length")
+p "string".respond_to?("reverse")
+p "string".respond_to?("odd")
+p "string".respond_to?("even")
+puts
+p 5.respond_to?("next")
+p 5.respond_to?("length")
+p 5.respond_to?("reverse")
+  
