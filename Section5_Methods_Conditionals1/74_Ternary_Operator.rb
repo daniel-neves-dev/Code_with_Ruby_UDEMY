30b5269aa7b5b2ea179371df936128014ebdd4b2
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-06 20:35:53 -0300

    Ternary operator

---
+++ Section5_Methods_Conditionals1/74_Ternary_Operator.rb
@@ -0,0 +1,18 @@
+
+def check_number(n)
+  n%2 == 0 ? "It is a even numer." : "It is a odd number"
+end
+
+puts check_number(5)
+puts check_number(6)
+
+puts 
+
+
+number = 5
+
+if number%2 == 0
+  puts "It is a even numer!"
+else
+  puts "It is a odd numer!"
+end
