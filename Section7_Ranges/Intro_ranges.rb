f9deeb3dea5287b907766c88f274bc2a8d86195c
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-08 16:29:55 -0300

    First range

---
+++ Section7_Ranges/Intro_ranges.rb
@@ -0,0 +1,16 @@
+def dynimic_range(lower, upper)
+  numbers = (lower..upper)
+  puts 
+  p numbers.first
+  p numbers.last
+  puts
+  p numbers.first(4)
+  p numbers.last(4)
+end
+
+puts "Tipe the first number:"
+lower = gets.to_i
+puts "Tipe the last number:"
+upper = gets.to_i
+
+dynimic_range(lower, upper)
