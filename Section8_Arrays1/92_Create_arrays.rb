713ca8385a1b9e3eeead072e1aa3092dd15109e1
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-08 17:42:40 -0300

    Arrays

---
+++ Section8_Arrays1/92_Create_arrays.rb
@@ -0,0 +1,13 @@
+numbers = [12,20,25,30,40]
+strings = %w[Boris Paulo Maria Paula]
+strings2 = ["Boris", "Paulo","maria","Paula"]
+things = ["Toys", 10.56, 2, true, false]
+array_inside_array = [["Paula", 25],
+                      ["Maria",30],
+                      ["Daniel",38]
+                      ]
+p numbers
+p strings
+p strings2
+p things
+p array_inside_array
