b5c4b91a56856f2770e2103e30938ef0cfe3d172
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-08 17:00:19 -0300

    Range size and includ method

---
+++ Section7_Ranges/89_Range_include.rb
@@ -0,0 +1,10 @@
+alphabet = "c".."h"
+p alphabet.include?("a") # can be used '==='
+p alphabet.include?("e")
+
+puts
+
+numbers = 1..10
+p numbers.include?(3)
+p numbers.include?(12)
+p numbers.size
