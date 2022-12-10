393ee5e5609ed235d8e09f73575257cdf6698666
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-10 16:47:32 -0300

    Spaceship operator

---
+++ Section8_Arrays1/110_Spaceship_operator.rb
@@ -0,0 +1,13 @@
+p 5 <=> 5 #return 0 (zero)
+p 5 <=> 3 #return positive 1
+p 5 <=> 8 #return negative 1
+p 5 <=> "string" #return nil (incomparable)
+
+puts
+p [1,2,3] <=> [1,2,3]
+p [1,2,3] <=> [1,2,0]
+p [1,2,3] <=> [1,2,6]
+p [1,2,3] <=> [1,2,"hello"]
+
+puts 
+p [1,2,3] <=> [1,2,3,4]
