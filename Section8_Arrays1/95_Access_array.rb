12c3161ec66cfba0c6d6a4995647a8708b5bb903
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-10 11:40:45 -0300

    Access index

---
+++ Section8_Arrays1/95_Access_array.rb
@@ -0,0 +1,11 @@
+fruits = ["apple", "orange","grape","banana","watermellon"]
+p fruits [0]
+p fruits[2]
+p fruits[-1]
+p fruits[fruits.length-1]
+puts 
+p fruits [10] #return NIL
+p fruits.fetch(10,"Fruit not in the list")#return a message.
+p fruits[0][3]
+p fruits [3][0]
+
---
+++ Section8_Arrays1/96_Index_array.rb
@@ -0,0 +1,24 @@
+def first_and_last(fruits)
+  puts "The first fruit of the list is: #{fruits[0]}"
+  puts "The last fruit of the list is: #{fruits[-1]}"
+end
+
+def firts_letter(fruits)
+  puts 
+  puts "The firts letter of the first fruit in the list is: #{fruits[0][0]}"
+  puts "The firts letter of the last fruit in the list is: #{fruits[-1][0]}"
+end
+
+def product_even_indicies(numbers)
+  puts 
+  puts "#{numbers[0]} x #{numbers[2]} x #{numbers[4]} = #{numbers[0]*numbers[2]*numbers[4]}"
+end
+
+fruits = ["apple", "orange","grape","banana","watermellon"]
+first_and_last(fruits)
+firts_letter(fruits)
+
+numbers = [1, 2, 3, 4, 5, 6]
+product_even_indicies(numbers)
+numbers = [3, 4, 3, 5, 3, 6]
+product_even_indicies(numbers)
