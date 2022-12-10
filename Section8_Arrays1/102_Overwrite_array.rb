5a550bd5cf5fe26f130be0746b1615ec5c7383d9
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-10 12:52:22 -0300

    Overwrite array

---
+++ Section8_Arrays1/102_Overwrite_array.rb
@@ -0,0 +1,14 @@
+fruits = ["apple", "orange", "grape", "banana"]
+p fruits
+
+fruits[0] = "pear"
+p fruits
+
+fruits[6] = "kiwi"
+p fruits
+
+fruits[1..4] = "blackberry", "peach", "cherry"
+p fruits
+
+fruits[0..2] = "newfruit"
+p fruits
