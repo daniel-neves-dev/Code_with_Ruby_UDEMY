3b44c8e72ea9f9ea0d513c53817136788dcfc62e
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-12 15:52:22 -0300

    Each method

---
+++ Section9_Arrays2/121_Eahc_method.rb
@@ -0,0 +1,21 @@
+puts
+candies = ["Sour Patch kids", "Milk Way","Airheads"]
+candies.each do |candy|
+  puts "I love eating #{candy}!!!"
+ end
+
+ puts 
+
+ puts "One line command"
+
+ names = ["daniel","paula","rodrigo"]
+ names.each {|name| puts "#{name.capitalize}"}
+
+ puts 
+
+ [1,2,3,4,5,6].each do |number|
+  square = number*number
+  puts "Thew square number of #{number} is #{square}" 
+ end
+
+ 
