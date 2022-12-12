ba08440559e515df5866e10ffa1087c81903bf58
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-12 19:12:47 -0300

    While and until loop

---
+++ Section9_Arrays2/128_While_until_loop.rb
@@ -0,0 +1,15 @@
+puts 
+animals = ["zebra", "baboon", "cheetah", "elephant", "kangaroo"]
+i = 0
+
+while i < animals.length
+  puts animals[i]
+  i += 1
+end
+
+puts 
+
+until i == animals.length
+  puts  animals[i]
+  i += 1
+end
