45eff2e1712eab978202dc027f9cbd2560982790
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-12 17:51:20 -0300

    Each with index

---
+++ Section9_Arrays2/125_Each_with_index.rb
@@ -0,0 +1,23 @@
+puts 
+colors = ["red", "blue", "black", "orange"]
+
+colors.each_with_index do |color, index|
+  puts "The color #{color} is in position #{index}"
+end
+
+puts 
+
+numbers = [5,10,15,20,25,30,35,40]
+numbers.each_with_index do |number, index|
+  puts "The current value is #{number} at index #{index}"
+  puts "#{number} x #{index} = #{number*index}"
+end
+
+puts 
+sum = 0
+[1,2,3,4,5].each_with_index do |num, i|
+  puts "#{num} + #{i} = #{num+i}"
+  sum += (num+i)
+end
+
+puts "The total is #{sum}"
