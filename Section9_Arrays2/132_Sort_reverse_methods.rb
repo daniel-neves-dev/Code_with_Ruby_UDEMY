1100f3dc7a94f0690081f16936e82ba8a7df40d1
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-12 20:02:12 -0300

    Sort and reverse

---
+++ Section9_Arrays2/132_Sort_reverse_methods.rb
@@ -0,0 +1,16 @@
+puts
+ 
+numbers = [5, 13, 1, -2, 8]
+words = ["caterpillar", "kangaroo", "apple", "zebra"]
+p numbers
+p words
+
+puts 
+
+p numbers.sort
+p words.sort
+
+puts
+
+p numbers.sort.reverse
+p words.sort.reverse
