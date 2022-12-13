1674852435f12e4972260ce1b6ced88ed176a9b1
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-13 09:16:14 -0300

    Min and max

--- Section9_Arrays2/134_Min_max_method.rb
+++ Section9_Arrays2/134_Min_max_method.rb
@@ -1,19 +1,38 @@
-puts 
+#puts 
 
-def custom_max(arr)
-  puts "O maior valor é: #{arr.max}"
+#def custom_max(arr)
+#  puts "O maior valor é: #{arr.max}"
+#end
+
+#def custom_min(arr)
+#  puts "O menor valor é: #{arr.min}"
+#end
+
+puts "ANOTHER WAY"
+puts
+
+def custom_max2 (arr)
+  max = arr[0]
+  arr.each do |value|
+    max = value if value > max
+    end
+    puts "O maior valor é: #{arr.max}"
 end
 
-def custom_min(arr)
+def custom_min2 (arr)
+  min = arr[0]
+  arr.each do |value|
+    min == value if value < min
+  end
   puts "O menor valor é: #{arr.min}"
 end
 
 arr_numbers = [5,3,8,4,9,7,2]
-custom_max(arr_numbers)
-custom_min(arr_numbers)
+custom_max2(arr_numbers)
+custom_min2(arr_numbers)
 
 puts
 
 arr_friuts = ["kiwi", "watermelon", "apple", "banana"]
-custom_max(arr_friuts)
-custom_min(arr_friuts)
+custom_max2(arr_friuts)
+custom_min2(arr_friuts)
