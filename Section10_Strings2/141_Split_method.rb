0270c9f41228e1e06dea56907c2b06a512899e76
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-13 11:47:35 -0300

    Split method

--- Section10_Strings2/141_Split_method.rb
+++ Section10_Strings2/141_Split_method.rb
@@ -2,3 +2,22 @@ puts
 sentence = "Hi, my name is Daniel. There are spaces here!"
 p sentence.split
 p sentence.split(".")
+
+puts 
+
+def longst_word(sentence)
+  word = ""
+  sentence.split.each do |long_word|
+    word = long_word if long_word.length > word.length
+  end
+  puts "The longest word of the sentence is '#{word}', with #{word.length} characthers"
+end
+
+sentence = "Hi, my name is Daniel. There are spaces here!"
+longst_word(sentence)
+
+sentence = "Bobby loves big scary kangaroos"
+longst_word(sentence)
+
+sentence = "Ruby is my favorite language"
+longst_word(sentence)
