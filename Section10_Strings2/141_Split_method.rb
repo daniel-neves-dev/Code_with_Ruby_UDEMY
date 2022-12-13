6dfcd5b1de88080a8511b55a2af89bd040b0692d
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-13 11:05:42 -0300

    Split method

---
+++ Section10_Strings2/141_Split_method.rb
@@ -0,0 +1,4 @@
+puts 
+sentence = "Hi, my name is Daniel. There are spaces here!"
+p sentence.split
+p sentence.split(".")
--- Section9_Arrays2/140_partition_method.rb
+++ Section9_Arrays2/140_partition_method.rb
@@ -2,3 +2,12 @@ foods = ["steak", "vegetables", "steak burger", "kale", "tofu", "tuna steaks"]
 steak, not_steak = foods.partition{|food| food.include?("steak")}
 p steak
 p not_steak
+
+puts 
+def even_odd_numbers(numbers)
+  even_n, odd_n = numbers.partition {|number| number.even?}
+  p even_n
+  p odd_n
+end
+numbers = [9,6,3,2,5,8,7,4,1]
+even_odd_numbers(numbers)
