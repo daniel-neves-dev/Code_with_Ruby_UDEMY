141ba457798c72b7416d579c6d272687e2f589d7
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-13 09:20:58 -0300

    Min and max

--- Section9_Arrays2/134_Min_max_method.rb
+++ Section9_Arrays2/134_Min_max_method.rb
@@ -1,31 +1,32 @@
 #puts 
+puts
 
 #def custom_max(arr)
-#  puts "O maior valor é: #{arr.max}"
+#  max = arr[0]
+#  arr.each do |value|
+#    max = value if value > max
+#    end
+#    puts "O maior valor é: #{arr.max}"
 #end
 
 #def custom_min(arr)
+#  min = arr[0]
+#  arr.each do |value|
+#    min == value if value < min
+#  end
 #  puts "O menor valor é: #{arr.min}"
 #end
 
-puts "ANOTHER WAY"
-puts
+puts "ANOTHER EASY WAY"
 
-def custom_max2 (arr)
-  max = arr[0]
-  arr.each do |value|
-    max = value if value > max
-    end
-    puts "O maior valor é: #{arr.max}"
+def custom_max2(arr)
+  puts "O maior valor é: #{arr.max}"
 end
 
-def custom_min2 (arr)
-  min = arr[0]
-  arr.each do |value|
-    min == value if value < min
-  end
+def custom_min2(arr)
   puts "O menor valor é: #{arr.min}"
 end
+
 
 arr_numbers = [5,3,8,4,9,7,2]
 custom_max2(arr_numbers)
