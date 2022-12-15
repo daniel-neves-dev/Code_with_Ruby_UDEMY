100288e090945fc4aa3bcc88bc3fb74795605376
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2022-12-15 11:26:32 -0300

    Flatten method

---
+++ Section11_Arrays_III/157_Flatten_method.rb
@@ -0,0 +1,7 @@
+registration = [ ["Bob", "Dan", "Jack"],
+                 ["Rick", "Susan", "Molly"],
+                 ["Pierce", "Sean", "George"]
+]
+p registration
+puts 
+p registration.flatten
